7daa5332c4de491770a39abedc0f20aabf8748e7
Marcelele <marceli.baczewski@gmail.com>
2016-05-10 23:49:23 +0200

    Add GUI and JSONReader stubs

---
+++ src/main/java/CookBook.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+public class CookBook {
+    Integer currentCategory;
+    Integer currentRecipe;
+    String currentView;
+    List<Category> categories;
+    RecipeDisplay recDisplay;
+    CategoryDisplay catDisplay;
+    MenuDisplay menuDisplay;
+
+    public static void main(String[] args) {
+        //loads categories
+        //initializes GUI components
+        //
+    }
+}
---
+++ src/main/java/JsonReader.java
@@ -0,0 +1,6 @@
+import java.util.List;
+
+interface JsonReader {
+    List<Category> readCategories();
+
+}
